f2cb2cef746f26bfe1b5c0bab3ff077f757b611d
Pablo Villagran Torres <pablo.villagran@liztex.com>
2018-08-24 15:07:31 -0600

    Toast y Snackbar samples

--- app/src/main/java/guideme/pvstudio/com/guideme/MainActivity.kt
+++ app/src/main/java/guideme/pvstudio/com/guideme/MainActivity.kt
@@ -2,11 +2,22 @@ package guideme.pvstudio.com.guideme
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.constraint.ConstraintLayout
+import android.support.design.widget.Snackbar
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, "Toast", Toast.LENGTH_LONG ).show() // Mensaje en pantalla
+
+        val layout = findViewById<ConstraintLayout>(R.id.mainView)
+        //Snackbar.make(layout, "SnackBar", Snackbar.LENGTH_LONG).show()
+        Snackbar.make(layout, "SnackBar", Snackbar.LENGTH_LONG).setAction("buttom"){
+            Snackbar.make(layout,"Clicked",Snackbar.LENGTH_LONG).show()
+        }.show()
     }
 }
