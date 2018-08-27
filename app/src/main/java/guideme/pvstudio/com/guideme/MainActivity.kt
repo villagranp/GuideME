43a6ef9380df17f54a009d5c90c418d84ac8a538
Pablo Villagran <josepablovillagran@gmail.com>
2018-08-27 09:39:58 -0600

    se agregan los botones para los ejemplos

--- app/src/main/java/guideme/pvstudio/com/guideme/MainActivity.kt
+++ app/src/main/java/guideme/pvstudio/com/guideme/MainActivity.kt
@@ -1,23 +1,31 @@
 package guideme.pvstudio.com.guideme
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
-import android.support.constraint.ConstraintLayout
 import android.support.design.widget.Snackbar
+import android.support.v7.app.AppCompatActivity
 import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setUpToolBar()
 
-        Toast.makeText(this, "Toast", Toast.LENGTH_LONG ).show() // Mensaje en pantalla
+        Toast.makeText(this, "Toast", Toast.LENGTH_LONG).show() // Mensaje en pantalla
 
-        val layout = findViewById<ConstraintLayout>(R.id.mainView)
+
+//        val layout = mainView
         //Snackbar.make(layout, "SnackBar", Snackbar.LENGTH_LONG).show()
-        Snackbar.make(layout, "SnackBar", Snackbar.LENGTH_LONG).setAction("buttom"){
-            Snackbar.make(layout,"Clicked",Snackbar.LENGTH_LONG).show()
+        Snackbar.make(mainContainer, "SnackBar", Snackbar.LENGTH_LONG).setAction("buttom") {
+            Snackbar.make(mainContainer, "Clicked", Snackbar.LENGTH_LONG).show()
         }.show()
+    }
+
+    fun setUpToolBar() {
+        setSupportActionBar(toolbar)
+//        supportActionBar.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title="Menu Kotlin"
     }
 }
