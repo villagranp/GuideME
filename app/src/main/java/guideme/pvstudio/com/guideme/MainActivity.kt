ac37b4be0276781b2ae33e09565834a4293897c0
Pablo Villagran <josepablovillagran@gmail.com>
2018-08-27 21:39:43 -0600

    sample Intent and Button listener for change activity

--- app/src/main/java/guideme/pvstudio/com/guideme/MainActivity.kt
+++ app/src/main/java/guideme/pvstudio/com/guideme/MainActivity.kt
@@ -1,5 +1,6 @@
 package guideme.pvstudio.com.guideme
 
+import android.content.Intent
 import android.os.Bundle
 import android.support.design.widget.Snackbar
 import android.support.v7.app.AppCompatActivity
@@ -12,12 +13,23 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setUpToolBar()
+        showToast()
+        showSnackBar()
 
+        val btnLifeCycle = btnCycle
+        val btnEvents = btnEvent
+
+        btnLifeCycle.setOnClickListener { goToLifeCycle() }
+        btnEvents.setOnClickListener{ goToEvents() }
+    }
+
+    fun showToast(){
         Toast.makeText(this, "Toast", Toast.LENGTH_LONG).show() // Mensaje en pantalla
+    }
 
-
+    fun showSnackBar(){
 //        val layout = mainView
-        //Snackbar.make(layout, "SnackBar", Snackbar.LENGTH_LONG).show()
+//        Snackbar.make(layout, "SnackBar", Snackbar.LENGTH_LONG).show()
         Snackbar.make(mainContainer, "SnackBar", Snackbar.LENGTH_LONG).setAction("buttom") {
             Snackbar.make(mainContainer, "Clicked", Snackbar.LENGTH_LONG).show()
         }.show()
@@ -28,4 +40,16 @@ class MainActivity : AppCompatActivity() {
 //        supportActionBar.setDisplayHomeAsUpEnabled(true)
         supportActionBar?.title="Menu Kotlin"
     }
+
+    private fun goToLifeCycle(){
+        val intent = Intent(this, lifeCycle::class.java)
+        startActivity(intent)
+
+    }
+    //LineFun
+    private fun goToEvents() = startActivity(Intent(this, Events::class.java))
+
+
+
+
 }
